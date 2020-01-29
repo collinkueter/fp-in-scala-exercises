0c36342197201bd2f0eedfcc1e6d21e44cce6eb4
Collin Kueter <collinkueter@gmail.com>
2020-01-28 19:48:00 -0600

    implemented option

--- src/main/scala/com/collinkueter/exercises/ChapterFour.scala
+++ src/main/scala/com/collinkueter/exercises/ChapterFour.scala
@@ -2,6 +2,33 @@ package com.collinkueter.exercises
 
 package com.collinkueter
 
-object ChapterFourExercises {
-    
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(get) => Some(f(get))
+    case None      => None
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case Some(get) => f(get)
+    case None      => None
+  }
+
+  def getOrElse[B >: A](default: => B): B = if (this == None) default else this.get
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = if (this == None) ob else this
+
+  def filter(f: A => Boolean): Option[A] = {
+    if (this == None) None
+    else if (f(this.get)) this
+    else None
+  }
+
+  def isDefined: Boolean = this != None
+
+  def get: A
+}
+
+case class Some[A](get: A) extends Option[A]
+case object None extends Option[Nothing] {
+  def get = throw new UnsupportedOperationException("get not defined on None")
 }
--- src/test/scala/com/collinkueter/exercises/ChapterFourTests.scala
+++ src/test/scala/com/collinkueter/exercises/ChapterFourTests.scala
@@ -5,9 +5,36 @@ package com.collinkueter
 import org.scalatest._
 
 class ChapterFourTests extends FlatSpec with Matchers with DiagrammedAssertions {
-  behavior of "chapter 4"
+  behavior of "Option"
 
-  it should "be true" in {
-    true shouldBe true
+  it should "map if exists" in {
+    Some(2).map(_ * 2) shouldBe Some(4)
+    None.map(f => f) shouldBe None
+  }
+
+  it should "flatmap if exists" in {
+    Some(2).flatMap(a => Some(a * 2)) shouldBe Some(4)
+    None.flatMap(f => f) shouldBe None
+  }
+
+  it should "getOrElse" in {
+    Some(2).getOrElse(1) shouldBe 2
+    None.getOrElse(1) shouldBe 1
+  }
+
+  it should "orElse" in {
+    Some(2).orElse(Some(1)) shouldBe Some(2)
+    None.orElse(Some(1)) shouldBe Some(1)
+  }
+
+  it should "filter" in {
+    Some(2).filter(a => a == 2) shouldBe Some(2)
+    Some(2).filter(a => a == 1) shouldBe None
+    None.filter(a => a == 2) shouldBe None
+  }
+
+  it should "isDefined" in {
+    Some(2).isDefined shouldBe true
+    None.isDefined shouldBe false
   }
 }
